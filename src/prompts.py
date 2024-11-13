bae3a2fdf7ec1b5f2f2b76b18a997e40ad170d07
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2024-11-13 19:58:08 +0900

    Create prompts.py

---
+++ src/prompts.py
@@ -0,0 +1,6 @@
+icl_ts = """Follow the steps to solve the given question.
+1. Translate the question to English and repeat it. 
+2. Breakdown the question to understand it.
+3. Solve it in English.
+4. Translate your solution back to Korean.
+5. State your final answer in the following format: $\\boxed{N}$."""
