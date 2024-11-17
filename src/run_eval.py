0a1ef76411a35db4a134849726246bfcb018fb97
Hyunwoo Ko <kopilot100@gmail.com>
2024-11-17 22:38:10 +0900

    Update run_eval.py

--- src/run_eval.py
+++ src/run_eval.py
@@ -8,10 +8,9 @@ from litellm import batch_completion
 from tqdm import tqdm
 import huggingface_hub
 
-huggingface_hub.login("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")
-
 # Set up argparse
 parser = argparse.ArgumentParser(description="Generate predictions and save results to CSV.")
+parser.add_argument('--token', type=str, required=True)
 parser.add_argument('--cats', nargs='+', default=['MATH', 'GSM8K', 'OMNI_MATH', "MMMLU", "KSM"],
                     help="List of dataset categories to process, separated by spaces.")
 parser.add_argument('--model_name', type=str, default='gpt-4o',
@@ -21,6 +20,7 @@ args = parser.parse_args()
 # Retrieve arguments
 cats = args.cats
 model_name = args.model_name
+huggingface_hub.login(args.token)
 
 # Load datasets
 dfs = {cat: pd.DataFrame(load_dataset('HAERAE-HUB/ksm', cat)['test']) for cat in cats}
