70a4c9adc045c1141a68d006f614c3a229f44a06
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2024-11-19 18:31:16 +0900

    update parse box func

--- src/data.py
+++ src/data.py
@@ -71,11 +71,10 @@ def convert_to_int_safe(string_number):
         
 def parse_boxed_value(text,answer):
     match = re.search(r'\\boxed\{(\d+[.,]?\d+)\}', str(text))
-    
     if match:
-        answer = convert_to_int_safe(match.group(1))
-        c1 = float(answer) == answer
-        c2 = str(answer) == str(answer)
+        pred = convert_to_int_safe(match.group(1))
+        c1 = float(pred) == answer
+        c2 = str(pred) == str(answer)
         return any([c1,c2])
     return False
 
