5e1727c8a3bacf4ec7d5894a4a9c7d0124fd0030
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2024-11-22 15:29:32 +0900

    Update prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -1,6 +1,12 @@
-icl_ts = """Follow the steps to solve the given question.
+prompts = {
+    "default":"",
+    "ko":"Repond in Korean.",
+    "en":"Repond in English.",
+    "TBST":"""Follow the steps to solve the given question.
 1. Translate the question to English and repeat it. 
 2. Breakdown the question to understand it.
 3. Solve it in English.
 4. Translate your solution back to Korean.
 5. State your final answer in the following format: $\\boxed{N}$."""
+}
+
