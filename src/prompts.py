9a585db75873168fcddcf90fdfb445c78afdcdc9
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2024-11-27 14:50:43 +0900

    Update prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -3,6 +3,7 @@ prompts = {
     "ko":"Repond in Korean.",
     "en":"Repond in English.",
     "oasst":"### Instruction:\n{instruction}\n\n### Response:\n",
+    "oasst_en":"### Instruction:\n{instruction}\n\n### Response:\n",
     "TBST":"""Follow the steps to solve the given question.
 1. Translate the question to English and repeat it. 
 2. Breakdown the question to understand it.
@@ -10,4 +11,3 @@ prompts = {
 4. Translate your solution back to Korean.
 5. State your final answer in the following format: $\\boxed{N}$."""
 }
-
