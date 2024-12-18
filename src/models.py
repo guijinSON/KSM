f81b454f554fbc80164175972cb3f7c09b3e4e3f
Hyunwoo Ko <80087878+gauss5930@users.noreply.github.com>
2024-12-18 22:39:10 +0900

    Update models.py

--- src/models.py
+++ src/models.py
@@ -9,6 +9,6 @@ litellm_models = [
     
 def load_vllm_model(model_name):
     llm = LLM(model_name, tensor_parallel_size=torch.cuda.device_count(),max_model_len=8192)
-    # params = SamplingParams(temperature=0.8, top_p=0.95, min_tokens=8, max_tokens=2048)
-    params = SamplingParams(temperature=0.0, min_tokens=8, max_tokens=2048)
+    params = SamplingParams(temperature=0.8, top_p=0.95, min_tokens=8, max_tokens=2048)
+    # params = SamplingParams(temperature=0.0, min_tokens=8, max_tokens=2048)
     return llm, params
